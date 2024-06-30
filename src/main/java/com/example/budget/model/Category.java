8032066164a25a2098191e3dbbf4beb112051d47
KrystynaCal <krystynacal5@gmail.com>
2024-06-30 13:29:38 +0200

    add import from jakarta.validation

--- src/main/java/com/example/budget/model/Category.java
+++ src/main/java/com/example/budget/model/Category.java
@@ -1,11 +1,16 @@
 package com.example.budget.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/example/budget/repository/CategoryRepository.java
+++ src/main/java/com/example/budget/repository/CategoryRepository.java
@@ -3,7 +3,6 @@ package com.example.budget.repository;
 import com.example.budget.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
