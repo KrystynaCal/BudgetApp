f46e480780e1b1448de6ec5c9f58be9894d302d5
KrystynaCal <krystynacal5@gmail.com>
2024-07-08 14:37:30 +0200

    add URI components - location header

--- src/main/java/com/example/budget/controller/TransactionController.java
+++ src/main/java/com/example/budget/controller/TransactionController.java
@@ -4,6 +4,8 @@ import com.example.budget.dto.TransactionCreateDto;
 import com.example.budget.dto.TransactionDto;
 import com.example.budget.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -22,8 +27,13 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
-    public TransactionDto createTransaction(@RequestBody TransactionCreateDto transactionCreateDto, @RequestParam Long categoryId) {
-        return transactionService.saveTransaction(transactionCreateDto, categoryId);
+    public ResponseEntity<TransactionDto> createTransaction(@RequestBody TransactionCreateDto transactionCreateDto, @RequestParam Long categoryId) {
+        TransactionDto transactionDto = transactionService.saveTransaction(transactionCreateDto, categoryId);
+        UriComponents uriComponents = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:8080/transactions/category/{categoryId}")
+                .buildAndExpand(categoryId);
+        return ResponseEntity.created(uriComponents.toUri())
+                .body(transactionDto);
     }
 
     @GetMapping
@@ -32,7 +42,8 @@ public class TransactionController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTransaction(@PathVariable long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTransaction(@PathVariable long id) {
         transactionService.deleteTransaction(id);
     }
 
