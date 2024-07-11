8ce81af9135537f4194e50b640786ff234f35a26
KrystynaCal <krystynacal5@gmail.com>
2024-07-11 21:02:39 +0200

    add thymeleaf

--- src/main/java/com/example/budget/controller/CategoryController.java
+++ src/main/java/com/example/budget/controller/CategoryController.java
@@ -5,6 +5,8 @@ import com.example.budget.dto.CategoryDto;
 import com.example.budget.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
@@ -29,8 +31,11 @@ public class CategoryController {
     }
 
     @GetMapping
-    public List<CategoryDto> getAllCategories() {
-        return categoryService.getAllCategories();
+    public String getAllCategories(Model model) {
+        List<CategoryDto> categoriesList = categoryService.getAllCategories();
+        System.out.println("Fetched Categories: " + categoriesList);
+        model.addAttribute("categoriesList", categoriesList);
+        return "home";
     }
 
     @GetMapping("/total-amount")
