0b13bb250303f819fa1716c2d2916347c5bc6414
KrystynaCal <krystynacal5@gmail.com>
2024-07-14 14:03:22 +0200

    add transactions form

--- src/main/java/com/example/budget/controller/TransactionController.java
+++ src/main/java/com/example/budget/controller/TransactionController.java
@@ -5,36 +5,32 @@ import com.example.budget.dto.TransactionDto;
 import com.example.budget.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
     private final TransactionService transactionService;
 
-    @PostMapping
-    public ResponseEntity<TransactionDto> createTransaction(@RequestBody TransactionCreateDto transactionCreateDto, @RequestParam Long categoryId) {
-        TransactionDto transactionDto = transactionService.saveTransaction(transactionCreateDto, categoryId);
-        UriComponents uriComponents = UriComponentsBuilder
-                .fromHttpUrl("http://localhost:8080/transactions/category/{categoryId}")
-                .buildAndExpand(categoryId);
-        return ResponseEntity.created(uriComponents.toUri())
-                .body(transactionDto);
+    @GetMapping("/create")
+    public String showCreateForm(@RequestParam Long categoryId, Model model) {
+        model.addAttribute("categoryId", categoryId);
+        return "create-transactions"; //
     }
+
+    @PostMapping("/create")
+    public String createTransaction(@ModelAttribute TransactionCreateDto transactionCreateDto, @RequestParam Long categoryId, Model model) {
+        model.addAttribute("categoryCreateDto", transactionCreateDto);
+        TransactionDto transactionDto = transactionService.saveTransaction(transactionCreateDto, categoryId);
+        model.addAttribute("categoryId", categoryId);
+        return "redirect:/categories/" + categoryId;
+    }
+
 
     @GetMapping
     public List<TransactionDto> getAllTransactions() {
